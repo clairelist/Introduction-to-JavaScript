d7c7e38b3c866f5b27622ed20ea990e9f486b783
Claire List <claire.list2501@gmail.com>
2021-09-07 20:19:11 -0600

    hacked together the rps game

--- index.js
+++ index.js
@@ -165,9 +165,36 @@ RULES OF THE GAME: Scissors beats Paper | Paper beats Rock | Rock beats Scissors
 
 HINT: Remember that the order in which we pass in our arguments matters when it comes to parameters
 */
+let computer = Math.random();
+
+if (computer <= 0.34){
+  computer= 'rock';
+} else if (computer <= 0.67){
+  computer = 'paper';
+} else if (compute > 0.67){
+  computer = 'scissors';
+}
 
 function game(user, computer){
-  /*add your code here*/
+ if (user === computer){
+   return 'its a tie';
+ } else if (user === 'rock' && computer === 'scissors'){
+   return 'you win!';
+ } else if (user === 'scissors' && computer === 'paper'){
+   return 'you win!';
+ } else if (user==='paper' && computer === 'rock'){
+   return 'you win!';
+  
+ } else if (user === 'scissors' && computer === 'rock'){
+   return 'you lose!';
+ } else if (user === 'rock' && computer === 'paper'){
+   return 'you lose!';
+ } else if (user === 'paper' && computer === 'scissors'){
+   return 'you lose!';
+  
+ } else {
+   return 'fatal error!';
+ }
 }
 
 
